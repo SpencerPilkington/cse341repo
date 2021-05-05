2cc87fadb35f33732c05c9796043f771beb61f5b
Spencer <spencer@foo.com>
2021-05-05 15:44:54 -0600

    commiting

--- index.js
+++ index.js
@@ -15,7 +15,6 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const path = require('path');
 const PORT = process.env.PORT || 5000 // So we can run on heroku || (OR) localhost:5000
-
 const app = express();
 
 // Route setup. You can implement more in the future!
@@ -42,6 +41,7 @@ app.use(express.static(path.join(__dirname, 'public')))
      // This is the primary index, always handled last. 
      res.render('pages/index', {title: 'Welcome to my CSE341 repo', path: '/'});
     })
+    .get('/', ta03Routes.processJson)
    .use((req, res, next) => {
      // 404 page
      res.render('pages/404', {title: '404 - Page Not Found', path: req.url})
--- routes/ta03.js
+++ routes/ta03.js
@@ -1,6 +1,10 @@
 //TA03 PLACEHOLDER
+const { response } = require('express');
 const express = require('express');
 const router = express.Router();
+const fs = require('fs');
+const http = require('http');
+
 
 router.get('/',(req, res, next) => {
     res.render('pages/ta03', { 
@@ -11,4 +15,28 @@ router.get('/',(req, res, next) => {
     });
 });
 
+function processJson(req, res) {
+    let url = 'https://byui-cse.github.io/cse341-course/lesson03/items.json';
+    http.get(url,function(response) {
+        var body ='';
+
+        response.on('data', function(chunk) {
+            body += chunk;
+        });
+
+        response.on('end', function(){
+            var jsonResponse = JSON.parse(body);
+            var stuff = {data:jsonResponse}
+
+            res.render('results', stuff);
+        });
+    }).on('error', function(e){
+        console.log("Got an error: ",e);
+    });
+}
+
+  
 module.exports = router;
+module.exports = {processJson: processJson};
+
+
