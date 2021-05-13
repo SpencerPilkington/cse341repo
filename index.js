3a6bdad68f1532810e9b3aa94de5dc521cdf8d7d
Spencer <spencer@foo.com>
2021-05-13 01:08:00 -0600

    connecting app with database

--- index.js
+++ index.js
@@ -15,6 +15,8 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const path = require('path');
 const PORT = process.env.PORT || 5000 // So we can run on heroku || (OR) localhost:5000
+const cors = require('cors') // Place this with other requires (like 'path' and 'express')
+const mongoose = require('mongoose');
 const app = express();
 
 // Route setup. You can implement more in the future!
@@ -35,7 +37,7 @@ app.use(express.static(path.join(__dirname, 'public')))
    // For view engine as hbs (Handlebars)
    //.engine('hbs', expressHbs({layoutsDir: 'views/layouts/', defaultLayout: 'main-layout', extname: 'hbs'})) // For handlebars
    //.set('view engine', 'hbs')
-   .use(bodyParser({extended: false})) // For parsing the body of a POST
+   .use(bodyParser.urlencoded({extended: false})) // For parsing the body of a POST
    .use('/ta01', ta01Routes)
    .use('/ta02', ta02Routes) 
    .use('/ta03', ta03Routes) 
@@ -61,4 +63,34 @@ app.use(express.static(path.join(__dirname, 'public')))
      // 404 page
      res.render('pages/404', {title: '404 - Page Not Found', path: req.url})
    })
-   .listen(PORT, () => console.log(`Listening on ${ PORT }`));
+   
+
+const corsOptions = {
+    origin: "https://cse341prove02projectapp.herokuapp.com/",
+    optionsSuccessStatus: 200
+};
+app.use(cors(corsOptions));
+
+const options = {
+    useUnifiedTopology: true,
+    useNewUrlParser: true,
+    useCreateIndex: true,
+    useFindAndModify: false,
+    family: 4
+};
+
+const MONGODB_URL = process.env.MONGODB_URL || "mongodb+srv://SpencerPilkington:Gs3d7fsbFSsdd122@cluster0.umk1b.mongodb.net/myFirstDatabase?retryWrites=true&w=majority";
+   
+mongoose
+  .connect(
+    MONGODB_URL, options
+  )
+  .then(result => {
+     // This should be your user handling code implement following the course videos
+    app.listen(PORT);
+  })
+  .catch(err => {
+    console.log(err);
+  });
+
+   //.listen(PORT, () => console.log(`Listening on ${ PORT }`));
