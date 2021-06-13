c2e24b4c40985318acbfd9aec5c53b5e8c668ecd
Spencer <spencer@foo.com>
2021-06-12 23:57:11 -0600

    does not work for prove03

--- routes/ta03.js
+++ routes/ta03.js
@@ -1,9 +1,29 @@
 //TA03 PLACEHOLDER
-const { response } = require('express');
+// const { response } = require('express');
+// const express = require('express');
+// const router = express.Router();
+// const fs = require('fs');
+// const http = require('http');
+
+
+// router.get('/',(req, res, next) => {
+//     res.render('pages/ta03', { 
+//         title: 'Team Activity 03', 
+//         path: '/ta03', // For pug, EJS 
+//         activeTA03: true, // For HBS
+//         contentCSS: true, // For HBS
+//     });
+// });
+
+// module.exports = router;
+
 const express = require('express');
 const router = express.Router();
 const fs = require('fs');
 const http = require('http');
+const fetch = require('node-fetch');
+
+const ITEMS_PER_PAGE = 10;
 
 
 router.get('/',(req, res, next) => {
@@ -14,8 +34,36 @@ router.get('/',(req, res, next) => {
         contentCSS: true, // For HBS
     });
 });
+router.get('/search',(req, res, next) => {
+    let URL = 'https://byui-cse.github.io/cse341-course/lesson03/items.json';
+    const productData = [];
+    const tag = req.body.tag;
+     fetch(URL)
+     .then(res => res.json())
+     .then((out) => {
+         if (productData != null) {
+             out.forEach(data => {
+                 productData.push(data)
+                 console.log('We made it in this part of the function!');
+             })
+         }
+         console.log(productData[0]);
+         let tagProducts = productData.filter((productData) => {
+             //Search for tag in a product's tag and add if found
+             return productData.tags.includes(tag);
 
+         });
+         res.render('pages/ta03', {
+            title: 'Team Activity 03',
+            path: '/ta03',
+            tagProducts: tagProducts,
+            currentPage: pageXOffset,
+            hasNextPage: ITEMS_PER_PAGE * page < totalItems,
+            nextPage: page + 1,
+            previousPage: page - 1,
+            lastPage: Math.ceil(totalItems/ ITEMS_PER_PAGE)
+        });
+     })
+     .catch(err => { throw err });
+});
 module.exports = router;
-
-
-
